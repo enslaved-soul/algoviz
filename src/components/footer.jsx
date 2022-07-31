ab805b7ec365acdad74c62a40bb3c3a669900e96
enslaved-soul <105877346+enslaved-soul@users.noreply.github.com>
2022-07-31 20:46:09 +0530

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created with ⚛️  by
+                
+                    <span> </span>
+                    Jatin Kumar
+            </p>
+        </footer>
+    );
+}
+ 
+export default Footer;
